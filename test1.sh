064caeb06bbe958676d5d583f1f71ddee42a5537
RevanthBSL <revanth98@gmail.com>
2018-08-27 19:51:59 +0530

    files added

---
+++ test1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+numb='10'
+echo "Pleae enter your name"
+read name
+read lastname
+echo "Welcome to Devops $name $lastname"
+echo "$name" * "$lastname"
+echo $name * $lastname
+echo $numb
+echo "$numb * 12"
